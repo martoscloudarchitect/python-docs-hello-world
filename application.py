f44b152a0d225cb8cfcb65791968617e330cf884
milindvb <mibelhe@microsoft.com>
2021-08-04 11:16:55 -0500

    Update application.py

--- application.py
+++ application.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from pyspark import SparkContext  
+from operator import add 
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    s = "Azure ! "
+    sc = SparkContext()  
+    data = sc.parallelize(list("Hello World"))  
+    counts = data.map(lambda x: (x, 1)).reduceByKey(add).sortBy(lambda x: x[1], ascending=False).collect()  
+    for (word, count) in counts:  
+        s = s + " , " + count 
+    sc.stop()
+    return s
