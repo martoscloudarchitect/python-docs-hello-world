b60bc79078efcf29b1a02fb63ad9c952efaac053
milindvb <mibelhe@microsoft.com>
2021-08-04 11:37:50 -0500

    Update application.py

--- application.py
+++ application.py
@@ -11,6 +11,6 @@ def hello():
     data = sc.parallelize(list("Hello World"))  
     counts = data.map(lambda x: (x, 1)).reduceByKey(add).sortBy(lambda x: x[1], ascending=False).collect()  
     for (word, count) in counts:  
-        s = s + " ,  " + count 
+        s = s + "  ,  " + count 
     sc.stop()
     return s
