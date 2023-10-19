ab4fff25324ba2ead4d7a766b63f28363e97f97d
milindvb <mibelhe@microsoft.com>
2023-10-19 11:00:31 -0500

    Update application.py
    reverting code

--- application.py
+++ application.py
@@ -14,35 +14,9 @@ cnxn = pyodbc.connect(f"DRIVER={driver};SERVER={server};DATABASE={database};UID=
 
 # Define the route for the web page
 @app.route("/")
-def PythonRetryLogicConnectToTheDB():
-    try:
-        nTimes=0
-        while nTimes <5:
-                nTimes=nTimes+1
-                print("Connecting to the DB - Attempt Number: %i " % nTimes)
-                start_time = time.time()    
-                conn = ConnectToTheDB()
-                if( conn != None ):
-                    print("Connected to the Database %s seconds ---" % ((time.time() - start_time)) )
-                    return conn    
-                else:
-                  print("------ Next Attempt ----- Waiting for 5 seconds ---")       
-                  time.sleep(5)     
-        return 
-    except Exception as e:
-        print("An error occurred connecting to the DB - " + format(e))
-        return
-        
-def ConnectToTheDB():
-    try:
-        return pyodbc.connect(f"DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password};timeout=30");  
-    except Exception as e:
-        print("An error occurred connecting to the DB - " + format(e))
-        return 
 
 def index():
     # Execute the SQL query and fetch the results
-    cnxn = PythonRetryLogicConnectToTheDB()
     cursor = cnxn.cursor()
     cursor.execute("SELECT TOP (1000) * FROM [SalesLT].[vGetAllCategories]")
     rows = cursor.fetchall()
